5e90259bc03aaeffd73ef87f7b7989bb3caa1a99
MarcPP <marc.padrell@estudiants.urv.cat>
2021-02-21 23:45:06 +0100

    Codi per mostrar data i hora, fet al main

--- src/cat/urv/deim/asm/labs/Main.java
+++ src/cat/urv/deim/asm/labs/Main.java
@@ -1,8 +1,16 @@
 package cat.urv.deim.asm.labs;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //Date data = new Date();
+
+        //System.out.print("0"+(data.getMonth()+1)+"/"+data.getDate()+"/"+ (data.getYear()+1900));
+        //System.out.print(" "+data.getHours()+":"+data.getMinutes()+":"+ data.getSeconds());
 
     }
 }
