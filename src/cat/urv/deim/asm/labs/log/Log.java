1f4a7da55d77c3a3d9d581a3c1cbf6f314073eea
Marc Domenech <marc.domenechg@estudiants.urv.cat>
2021-02-22 21:49:39 +0100

    eliminades parts del codi redundants per tal de polir-lo

--- src/cat/urv/deim/asm/labs/log/Log.java
+++ src/cat/urv/deim/asm/labs/log/Log.java
@@ -11,31 +11,31 @@ public class Log implements ILog, IColor {
     }
 
     public static void e(String tag, String message) {
-        consoleMessage(ANSI_RED,ANSI_RED+tag+ANSI_RESET, ANSI_RED+message+ANSI_RESET, Log.ERROR);
+        consoleMessage(ANSI_RED,tag, message+ANSI_RESET, Log.ERROR);
     }
 
     public static void w(String tag, String message){
-        consoleMessage(ANSI_ORANGE,ANSI_ORANGE+tag, ANSI_ORANGE+message+ANSI_RESET, Log.WARNING);
+        consoleMessage(ANSI_ORANGE,tag, message+ANSI_RESET, Log.WARNING);
     }
 
     public static void d(String tag, String message){
 
-        consoleMessage(ANSI_BLUE,ANSI_BLUE+tag, ANSI_BLUE+message+ANSI_RESET, Log.DEBUG);
+        consoleMessage(ANSI_BLUE,tag, message+ANSI_RESET, Log.DEBUG);
     }
 
     public static void i(String tag, String message){
 
-        consoleMessage(ANSI_BLACK,ANSI_BLACK+tag, ANSI_BLACK+message+ANSI_RESET, Log.INFO);
+        consoleMessage(ANSI_BLACK,tag, message+ANSI_RESET, Log.INFO);
     }
 
     public static void v(String tag, String message){
-        consoleMessage(ANSI_BLACK,ANSI_BLACK+tag, ANSI_BLACK+message+ANSI_RESET, Log.VERBOSE);
+        consoleMessage(ANSI_BLACK,tag, message+ANSI_RESET, Log.VERBOSE);
     }
 
 
     @Override
     public void error(String tag, String message) {
-        Log.e(ANSI_RED+tag, ANSI_RED+message+ANSI_RESET);
+        Log.e(ANSI_RED+tag, message+ANSI_RESET);
     }
 
     @Override
@@ -46,16 +46,16 @@ public class Log implements ILog, IColor {
 
     @Override
     public void debug(String tag, String message) {
-        Log.d(ANSI_BLUE+tag, ANSI_BLUE+message+ANSI_RESET);
+        Log.d(ANSI_BLUE+tag, message+ANSI_RESET);
     }
 
     @Override
     public void info(String tag, String message) {
-        Log.i(ANSI_BLACK+tag, ANSI_BLACK+message+ANSI_RESET);
+        Log.i(ANSI_BLACK+tag, message+ANSI_RESET);
     }
 
     @Override
     public void verbose(String tag, String message) {
-        Log.v(ANSI_BLACK+tag, ANSI_BLACK+message+ANSI_RESET);
+        Log.v(ANSI_BLACK+tag, message+ANSI_RESET);
     }
 }
